7371a63c3eb8f330124bf733b26754a09c56c55a
izola1 <isaacmagaji1@gmail.com>
2021-01-04 21:19:41 +0100

    Refactoring code and adding project assets

---
+++ src/containers/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Card from "../components/card/Card";
+class List extends Component {
+    render() {
+        return (
+            <div>
+                <Card/>
+            </div>
+        )
+    }
+}
+
+export default List;
