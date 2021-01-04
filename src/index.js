3e99eeed41f94315d5871b6b9df0a66f82c53129
izola1 <isaacmagaji1@gmail.com>
2021-01-04 15:09:26 +0100

    creating a development server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Welcome moveList')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList</h1>;
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
